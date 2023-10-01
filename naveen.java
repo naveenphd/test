8b53127bd152eea3fdd272e68800026e6ff1523a
naveenphd <naveen2phd@hotmail.com>
2023-10-01 18:05:31 +0530

    Update naveen.java

--- naveen.java
+++ naveen.java
@@ -1,7 +1,7 @@
 public class naveen{
     public static void main(String[] args){
         System.out.println("hello world1");
-        int i=10;
+        int i=20;
         for(int i=0;i<=10;i++){
          System.out.println("i value is:" i)
         }
