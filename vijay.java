c2b0225e8bb6bec1b1feefbce80cd8ea45ebfa84
naveen <naveen2phd@hotmail.com>
2023-10-01 17:53:24 +0530

    commit

---
+++ vijay.java
@@ -0,0 +1,10 @@
+public class vijay{
+    public static void main(String[] args){
+        System.out.println("hello world1");
+        int i=10;
+        for(int i=0;i<=10;i++){
+         System.out.println("i value is:" i)
+        }
+        
+    }
+}
